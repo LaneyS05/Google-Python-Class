b55292843bf049b4b7ea255109dbd109e2b3827e
LaneyS05 <laney.staggs05@gmail.com>
2024-10-09 14:51:22 -0400

    commands

---
+++ OS/Module4/streams.py
@@ -0,0 +1,7 @@
+data = input("This will come from STDIN: ")
+print("Now we write it to STDOUT: " + data)
+print("Now we generate an error to STDERR: " + data + 1)
+
+data = input("This will come from STDIN: ")
+print("Now we write it to STDOUT: " + data)
+print("Now we generate an error to STDERR: " + data + 1)
